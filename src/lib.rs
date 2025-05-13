dddd09bf369edb5a26e259aaaf52e9455ee529e6
Alex Harrison <alexander.harrison.r@gmail.com>
2025-05-12 23:53:26 -0600

    get travel time violations and controller type detection working

---
+++ src/lib.rs
@@ -0,0 +1,211 @@
+use slp_parser::{Game, VectorI8};
+use std::collections::HashSet;
+
+pub type ViolationMask = u64;
+pub mod violation {
+    use super::ViolationMask;
+    pub const TRAVEL_TIME: ViolationMask = 1 << 0;
+    
+    pub const ALL               : ViolationMask = TRAVEL_TIME;
+    pub const RAW_COORD_REQUIRED: ViolationMask = TRAVEL_TIME;
+}
+
+pub fn violation_name(mask: ViolationMask) -> &'static str {
+    match mask {
+        violation::TRAVEL_TIME => "Travel time",
+        _ => "Unknown",
+    }
+} 
+
+#[derive(Copy, Clone, Debug, PartialEq)]
+pub enum StickType { Analog, Digital }
+
+#[derive(Clone, Debug)]
+pub struct PlayerViolations {
+    /// Bitmask of which violations were detected for this player.
+    pub found: ViolationMask,
+    
+    /// Whether the lstick is analog or digital.
+    pub lstick_type: StickType,
+    
+    /// The percentage of control stick movements that do not have intermediate values.
+    ///
+    /// Between 0 and 1.
+    /// Values less than 0.25 will count as a violation. 
+    pub travel_time_hit_rate: f32,
+}
+
+impl PlayerViolations {
+    pub const DEFAULT: PlayerViolations = PlayerViolations {
+        found: 0,
+        lstick_type: StickType::Analog,
+        
+        travel_time_hit_rate: 0.0,
+    };
+}
+
+#[derive(Clone, Debug)]
+pub struct ViolationResult {
+    /// Bitmask of which violations were checked.
+    ///
+    /// Some violations cannot be checked because the slp file is too old and
+    /// does not contain the necessary information.
+    ///
+    /// Requirements:
+    /// - Travel time: slp version >= 3.17.0
+    pub checked: ViolationMask,
+    
+    /// Bitmask of which violations were skipped.
+    pub skipped: ViolationMask,
+    
+    /// Violations for each player.
+    ///
+    /// If the player does not exist, then there will be no violations.
+    pub players: [PlayerViolations; 4],
+}
+
+impl ViolationResult {
+    pub const DEFAULT: ViolationResult = ViolationResult {
+        checked: 0,
+        skipped: 0,
+        players: [
+            PlayerViolations::DEFAULT,
+            PlayerViolations::DEFAULT,
+            PlayerViolations::DEFAULT,
+            PlayerViolations::DEFAULT,
+        ]
+    };
+}
+
+pub fn iter_violations(mask: ViolationMask) -> impl Iterator<Item=ViolationMask> {
+    let mut mask = mask;
+    std::iter::from_fn(move || {
+        let bit = mask.trailing_zeros();
+        if bit == ViolationMask::BITS { return None; }
+        
+        let next = 1 << bit;
+        mask ^= next;
+        Some(next)
+    })
+}
+
+pub fn check_game(game: &Game) -> ViolationResult {
+    let mut res = ViolationResult::DEFAULT;
+    
+    if game.info.min_version(3, 17, 0) {
+        res.checked |= violation::RAW_COORD_REQUIRED;
+    } else {
+        res.skipped |= violation::RAW_COORD_REQUIRED;
+    }
+    
+    let mut lstick_coords = Vec::new();
+    let mut rim_coords = std::collections::HashSet::<(i8, i8)>::with_capacity(4096);
+    
+    for ply in 0..4 {
+        let Some(ref frames) = game.frames[ply] else { continue; };
+        let vio = &mut res.players[ply];
+        
+        // push clamped coords to buffer
+        lstick_coords.clear();
+        lstick_coords.reserve(frames.len());
+        for f in frames {
+            lstick_coords.push(f.left_stick_coords_raw.clamped());
+        }
+        
+        // determine digital/analog lstick
+        vio.lstick_type = stick_type(&mut rim_coords, &lstick_coords);
+        
+        // skip some checks if analog
+        if vio.lstick_type == StickType::Digital {
+            if res.checked & violation::TRAVEL_TIME != 0 {
+                vio.travel_time_hit_rate = travel_time_hit_rate(&lstick_coords);
+                if vio.travel_time_hit_rate < 0.25 {
+                    vio.found |= violation::TRAVEL_TIME;
+                }
+            }
+        }
+    }
+    
+    res
+}
+
+// altered from https://github.com/altf4/libenforcer/blob/9e8c3dabba8d42d30cbe513dfbe09c0e72bd989d/src/index.ts#L291
+fn stick_type(
+    rim_coords: &mut HashSet<(i8, i8)>,
+    coords: &[VectorI8],
+) -> StickType {
+    let coords_len = coords.len();
+    rim_coords.clear();
+    const RIM: isize = (VectorI8::MAX as isize - 1).pow(2);
+    
+    for coord in coords.iter().copied() {
+        let x_sq = (coord.x as isize).pow(2);
+        let y_sq = (coord.y as isize).pow(2);
+        if x_sq + y_sq >= RIM {
+            rim_coords.insert((coord.x, coord.y));
+        }
+    }
+    
+    const RIM_COORD_MAX: f32 = 432.0;
+    let mut rim_proportion = rim_coords.len() as f32 / RIM_COORD_MAX;
+    
+    const THREE_MINUTES: usize = 10800;
+    if coords_len < THREE_MINUTES {
+        let boost = (THREE_MINUTES - coords_len) as f32 / THREE_MINUTES as f32 + 1.0;
+        rim_proportion *= boost;
+    }
+    
+    if rim_proportion < 0.5 {
+        StickType::Digital
+    } else {
+        StickType::Analog
+    }
+}
+
+// altered from https://github.com/altf4/libenforcer/blob/9e8c3dabba8d42d30cbe513dfbe09c0e72bd989d/src/travel_time.ts#L20
+fn travel_time_hit_rate(coords: &[VectorI8]) -> f32 {
+    let mut travel_coord_count = 0;
+    let mut target_count = 0;
+    let mut last_coord = VectorI8 { x: 0, y: 0 };
+    let mut is_target = true;
+    let mut is_travel = false;
+    
+    for coord in coords.iter().copied() {
+        if coord == last_coord {
+            if !is_target {
+                target_count += 1;
+            }
+            is_target = true;
+            is_travel = false;
+        } else {
+            if !is_target && !is_travel {
+                travel_coord_count += 1;
+                is_travel = true;
+            }
+            is_target = false;
+        }
+        last_coord = coord;
+    }
+    
+    if target_count <= 1 {
+        return 1.0;
+    }
+    travel_coord_count as f32 / (target_count - 1) as f32
+}
+
+#[test]
+fn test() {
+    // p1_no_nerf_box ---------------------------------------
+    // p1: box with travel time violation
+    // p2: gcc with no violations
+    
+    let (game, _) = slp_parser::read_game(std::path::Path::new("test_slps/p1_no_nerf_box.slpz")).unwrap();
+    let res = check_game(&game);
+    
+    assert_eq!(res.checked, violation::ALL);
+    assert_eq!(res.skipped, 0);
+    assert_eq!(res.players[0].found, violation::TRAVEL_TIME);
+    assert_eq!(res.players[0].lstick_type, StickType::Digital);
+    assert_eq!(res.players[1].found, 0);
+    assert_eq!(res.players[1].lstick_type, StickType::Analog);
+}
---
+++ src/main.rs
@@ -0,0 +1,58 @@
+
+pub use slp_enforcer::*;
+
+fn main() -> std::process::ExitCode {
+    let path = match std::env::args().nth(1) {
+        Some(path) => path,
+        None => {
+            print_usage();
+            return std::process::ExitCode::FAILURE;
+        }
+    };
+    
+    use slp_parser::SlpError::*;
+    let game = match slp_parser::read_game(std::path::Path::new(&path)) {
+        Ok((game, _)) => game,
+        Err(e) => {
+            eprintln!("Error opening {}:", path);
+            match e {
+                OutdatedFile => eprintln!(
+                    "Slp file is too old! Slp enforcer requires a version >= v{}.{}.{}",
+                    slp_parser::MIN_VERSION_MAJOR, slp_parser::MIN_VERSION_MINOR, 0,
+                ),
+                TooNewFile => eprintln!("Slp file is too new! Please update slp enforcer."),
+                NotAnSlpFile => eprintln!("File is not a valid slp or slpz file."),
+                InvalidFile(_) => eprintln!("File is corrupt or malformed."),
+                ZstdInitError => eprintln!("Failed to initialize zstd decompressor."),
+                FileDoesNotExist => eprintln!("File does not exist."),
+                IOError => eprintln!("Could not open or read file.")
+            }
+            
+            return std::process::ExitCode::FAILURE;
+        }
+    };
+    
+    let violations = check_game(&game);
+    
+    for ply in 0..4 {
+        let found = violations.players[ply].found;
+        if found != 0 {
+            let mut name = String::new();
+            slp_parser::decode_shift_jis(&game.info.names[ply], &mut name);
+            println!("{} fails!", &name);
+            for violation in iter_violations(found) {
+                println!("  {} violated", violation_name(violation));
+            }
+        }
+    }
+    
+    std::process::ExitCode::SUCCESS
+}
+
+fn print_usage() {
+    print!(
+"USAGE:
+    slp_enforcer <path/to/file>
+"
+    )
+}
